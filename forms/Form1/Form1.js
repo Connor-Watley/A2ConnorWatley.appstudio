615a91f9e1d5d0ce06470c555e6d8a6ff823b468
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-02-25 19:50:19 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,12 +1,12 @@
 
 /*declared variables*/
-let myState = prompt("enter a two-letter, capitalized state");
+/*let myState = prompt("enter a two-letter, capitalized state");
 let myTemp = prompt ("enter the current outside temperature in Fahrenheit");
 let messages = ["wear a warm coat, hat, scarf and gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat, and gloves. Maybe a scarf too."]
-let myName = "Mary";
+let myName = "Mary";*/
 
 /*set up the cases*/
-switch (true){
+/*switch (true){
 case myState = "NE" && myTemp < 32:
    console.log(`${myName}, ${messages[0]}`);
    break;
@@ -23,3 +23,6 @@ case myState = "FL" && myTemp > 50 && myTemp <= 70:
    console.log(`${myName}, ${messages[3]}`);
    break;
 }
+}*/
+
+
